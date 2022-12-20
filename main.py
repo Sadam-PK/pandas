303a272f00722cfbcb8a06f3deee5a92868ce746
sadam <sadamccr@gmail.com>
2022-12-21 02:08:43 +0500

    series in pandas

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+labels = ['a', 'b', 'c']
+
+numArr = np.array([10, 20, 30])
+
+dict = {'a': 10, 'b': 20, 'c': 30}
+
+# print(pd.Series(labels))
+# print(pd.Series(numArr))
+# print(pd.Series(dict))
+
+s = pd.Series(numArr, labels)
+print(s)
