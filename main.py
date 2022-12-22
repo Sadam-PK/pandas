9f5cdf404aa986e59a99519286698c3048bc9208
sadam <sadamccr@gmail.com>
2022-12-22 13:21:40 +0500

    dataframe file added and frames create, updated and drop done

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-import numpy as np
-import pandas as pd
+# import numpy as np
 
-labels = ['a', 'b', 'c']
-
-numArr = np.array([10, 20, 30])
-
-dtnry = {'a': 10, 'b': 20, 'c': 30}
-
-# print(pd.Series(labels))
-# print(pd.Series(numArr))
-# print(pd.Series(dtnry))
-
-s = pd.Series(numArr, labels)
-print(s)
+#
+# labels = ['a', 'b', 'c']
+#
+# numArr = np.array([10, 20, 30])
+#
+# dtnry = {'a': 10, 'b': 20, 'c': 30}
+#
+# # print(pd.Series(labels))
+# # print(pd.Series(numArr))
+# # print(pd.Series(dtnry))
+#
+# s = pd.Series(numArr, labels)
+# print(s)
+#
+# Ser1 = pd.Series([1, 2, 3, 4], ['usa', 'china', 'ussr', 'japan'])
+# Ser2 = pd.Series([1, 2, 5, 4], ['usa', 'germany', 'ussr', 'japan'])
+#
+# print(Ser1 + Ser2)
